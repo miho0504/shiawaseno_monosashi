f1c84b43132294bbdf68b451b5aff585bb072a4f
EC2 Default User <ec2-user@ip-172-31-13-151.ec2.internal>
2022-02-25 04:15:19 +0000

    Fix:投稿制限の修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,18 @@ class Post < ApplicationRecord
   
   validates :today_mood, presence: true
   validates :content, length: { maximum: 30 } 
+  
+  # 1日1回迄の投稿制限
+  validate :diary_limit, on: :create
+  
+  # 今日の感情表示
   enum today_mood: { ⭕️: 0, 🔺: 1, ❌: 2 }
-  validate :diary
   
   private
-
-  def diary
-    if Post.where('created_at >= ?', Date.today).present?
+  
+  # 1日1回迄の投稿制限
+  def diary_limit
+    if Post.where(user_id: user.id).where("start_time > ?", Date.today).present?
       errors.add(:content,"の投稿は1日1回までです")
     end
   end
