fa66890e532df2c7f1954b0a72a15d8a5c5a2c75
EC2 Default User <ec2-user@ip-172-31-13-151.ec2.internal>
2022-02-26 09:54:11 +0000

    Add:relationshipモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,31 @@
 class User < ApplicationRecord
+  
+  # ユーザーとの投稿アソシエーション
   has_many :posts, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  # ユーザーフォロー関係
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
+  
+  # ユーザーランダムIDの生成
+  generate_public_uid
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  generate_public_uid
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_072344) do
+ActiveRecord::Schema.define(version: 2022_02_26_094025) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2022_02_20_072344) do
     t.integer "today_mood", null: false
     t.integer "user_id"
     t.datetime "start_time"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
   create_table "users", force: :cascade do |t|
