152d216f024a2eb2ffe8765f25c9ec2d76df78ee
EC2 Default User <ec2-user@ip-172-31-13-151.ec2.internal>
2022-03-18 09:32:56 +0000

    Add:シェアボタン・フォローボタンを友達詳細ページへ移動

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,13 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find_by(id: params[:relationship][:following_id])
     current_user.follow!(@user)
-    redirect_to users_path
+    redirect_to followers_path(@user)
   end
 
   def destroy
     @user = Relationship.find(params[:id]).following
     current_user.unfollow!(@user)
-    redirect_to users_path
+    redirect_to followers_path(@user)
   end
 end
 
