acdbf7a9b32c3c0d816b4978a60abf4990052d69
EC2 Default User <ec2-user@ip-172-31-13-151.ec2.internal>
2022-04-19 11:23:15 +0000

    Add:機能追加

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -3,17 +3,17 @@ class GraphsController < ApplicationController
   def index
     @posts = Post.all
     @posts = Post.where(user_id: current_user.id)
-    # その月のみ表示の為にはその月の投稿を抽出
-    @month = Date.today.month
+    
     # 評価比較
     @maru = @posts.where(today_mood: "◯")
     @sankaku = @posts.where(today_mood: "△")
     @batu = @posts.where(today_mood: "×")
+    
     # グラフ
-    # 変数の生成部が1つのブロック
     @day_list = {}.tap do |result|
       @posts.each do |post|
         # 今日の日付と値に対する処理
+        # 投稿が一致するものを複数から探す
         result[post.created_at.to_date] = case post.today_mood
                                   when '○'
                                     3
@@ -24,11 +24,22 @@ class GraphsController < ApplicationController
                                   end
       end
     end
+    # 今月を取得し月を表示
+    @month = Date.today
     
-    # 月データ範囲 月初から月末 配列に変換
-    @month_days = (Date.today.beginning_of_month..Date.today.end_of_month).to_a
+    # パラメーターでdate月を取得した時の処理
+    @month_days =  if params[:date]
+                    # 表示月は今月の前月にする
+                     @month = Date.today
+                    # 月初と月末まで parseで日付をオブジェクトに変換
+                     (Date.today.beginning_of_month..Date.today.end_of_month).to_a
+                   else
+                     @month = Date.today.next_month
+                      # 月データ範囲 月初から月末 配列に変換
+                     (Date.today.beginning_of_month..Date.today.end_of_month).to_a
+                   end
     
-    
+    # 
     @post_by_day = {}.tap do |result|
       @month_days.each do |d|
         result[d] = @day_list[d] ? @day_list[d] : 0
