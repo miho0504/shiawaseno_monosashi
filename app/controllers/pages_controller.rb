962d0c3caec44d2b73316d389c0ad31ce4b67275
EC2 Default User <ec2-user@ip-172-31-13-151.ec2.internal>
2022-04-22 07:05:52 +0000

    Fix:ゲストログインの修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   before_action :sign_in_required, only: [:show]
   
-  def index
+  def index;end
+  
+  def guest_sign_in
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end
 
